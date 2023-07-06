bef10098aaa9e276eb777965a1daaf69d885614c
nytakoe1 <yana.pavlova.pole@gmail.com>
2023-07-06 16:02:51 +0300

    Initial Commit

---
+++ lab5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lab5
+//
+//  Created by Яна Павлова on 06.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
